c4af7dc99b37afba9f7699438438e99d3c7aa753
Saremox <saremox@linux.com>
2014-06-18 19:40:29 +0200

    First implementation

---
+++ src/de/saremox/swing/JNumberField.java
@@ -0,0 +1,69 @@
+package de.saremox.swing;
+
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+public class JNumberField extends JTextField
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6286786630441804971L;
+	
+	public JNumberField(int cols)
+	{
+		super(cols);
+	}
+
+	
+
+	protected Document createDefaultModel()
+	{
+		return new NumberDocument();
+	}
+
+	static class NumberDocument extends PlainDocument
+	{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1594158457704096352L;
+		
+		
+		public NumberDocument()
+		{
+			super();
+		}
+		
+		public void insertString(int offs, String str, AttributeSet a)
+				throws BadLocationException
+		{
+			if (str == null)
+			{
+				return;
+			}
+			char[] numbers = str.toCharArray();
+			int lenght = numbers.length;
+			for (int i = 0; i < numbers.length; i++)
+			{
+				if(!Character.isDigit(numbers[i]))
+				{
+					for (int copy = 1;copy < numbers.length-1; copy++)
+					{
+						numbers[copy] = numbers[copy+1];
+					}
+					numbers[numbers.length-1] = 0x00;
+					lenght--;
+				}
+				
+			}
+			
+			super.insertString(offs, new String(numbers, 0, lenght), a);
+		}
+		
+	}
+}
